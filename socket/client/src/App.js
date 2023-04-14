0ad4207697335b1e4127d675e08dcf0811f91492
Logical_IT <>
2023-04-14 13:52:13 +0900

    feat : client input placeholder 일부 수정

--- socket/client/src/App.js
+++ socket/client/src/App.js
@@ -22,10 +22,10 @@ function App() {
     {!showChat ? (    
     <div className="joinChatContainer">
     <h3>Join A chat</h3>
-    <input type="text" placeholder="John..." onChange={(event) => {
+    <input type="text" placeholder="이름을 입력하세요." onChange={(event) => {
       setUsername(event.target.value)
     }}/>
-    <input type="text" placeholder="Room ID..."onChange={(event) => {
+    <input type="text" placeholder="입장하실 채팅방을 입력하세요"onChange={(event) => {
       setRoom(event.target.value)
     }} />
     <button onClick={joinRoom}>Join A Room</button>
